d4f6ca8299686f49706a040030d477e3d8455f49
Rajagopalan R <32943@Hexaware.com>
2016-09-06 21:50:00 +0530

    code not commented

--- full_release.ps1
+++ full_release.ps1
@@ -122,5 +122,5 @@ if(!(Test-Path -Path $computers)){
 $list = Get-Content $computers 
 
 foreach($element in $list) { write $element
-#copy-item $build_dir1 -Destination \\$element\$build_dir\ -recurse # copy the sdc build across all app servers
+copy-item $build_dir1 -Destination \\$element\$build_dir\ -recurse # copy the sdc build across all app servers
 }
